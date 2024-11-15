7f26ccc3ca5e8260d286e08989ffeb7e6b7b10cf
Rijad Halili <rijadhalili.yt@gmail.com>
2024-11-15 18:44:55 +0100

    Update DepartamentPort.js

--- Hospital Management System/backend/ports/DepartmentPort.js
+++ Hospital Management System/backend/ports/DepartmentPort.js
@@ -1,31 +1,34 @@
-// DepartmentPort.js
 const DepartmentService = require("../core/services/DepartmentServices");
 
 class DepartmentPort {
+    constructor(departmentService) {
+        this.departmentService = departmentService;
+    }
     async findAllDepartments() {
         console.log("Calling DepartmentService.findAllDepartments");
-        return await DepartmentService.findAllDepartments();
+        return await this.departmentService.findAllDepartments();
     }
 
     async findSingleDepartment(departmentId) {
         console.log(`Calling DepartmentService.findSingleDepartment with ID: ${departmentId}`);
-        return await DepartmentService.findSingleDepartment(departmentId);
+        return await this.departmentService.findSingleDepartment(departmentId);
     }
 
     async addDepartment(departmentData) {
         console.log("Calling DepartmentService.addDepartment with data:", departmentData);
-        return await DepartmentService.addDepartment(departmentData);
+        return await this.departmentService.addDepartment(departmentData);
     }
 
     async updateDepartment(departmentId, departmentData) {
         console.log(`Calling DepartmentService.updateDepartment with ID: ${departmentId} and data:`, departmentData);
-        return await DepartmentService.updateDepartment(departmentId, departmentData);
+        return await this.departmentService.updateDepartment(departmentId, departmentData);
     }
 
     async deleteDepartment(departmentId) {
         console.log(`Calling DepartmentService.deleteDepartment with ID: ${departmentId}`);
-        return await DepartmentService.deleteDepartment(departmentId);
+        return await this.departmentService.deleteDepartment(departmentId);
     }
 }
 
-module.exports = new DepartmentPort();
+module.exports = new DepartmentPort(DepartmentService);
+
