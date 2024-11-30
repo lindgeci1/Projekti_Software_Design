2a195e51b70f355678130d1fa0398785da672a65
Rijad Halili <rijadhalili.yt@gmail.com>
2024-11-30 16:41:44 +0100

    Payroll Routes fixed and updated

--- Hospital Management System/backend/routes/PayrollRoutes.js
+++ Hospital Management System/backend/routes/PayrollRoutes.js
@@ -9,9 +9,9 @@ class PayrollRoutes {
     }
 
     initializeRoutes() {
-        this.router.get("/payroll", authenticateToken(['admin', 'doctor', 'patient']), this.controller.findAllPayrolls.bind(this.controller));
-        this.router.get("/payroll/:id", authenticateToken(['admin', 'doctor', 'patient']), this.controller.findSinglePayroll.bind(this.controller));
-        this.router.post("/payroll/create", authenticateToken(['admin', 'doctor', 'patient']), this.controller.addPayroll.bind(this.controller));
+        this.router.get("/payroll", authenticateToken(['admin', 'doctor']), this.controller.findAllPayrolls.bind(this.controller));
+        this.router.get("/payroll/:id", authenticateToken(['admin', 'doctor']), this.controller.findSinglePayroll.bind(this.controller));
+        this.router.post("/payroll/create", authenticateToken(['admin', 'doctor']), this.controller.addPayroll.bind(this.controller));
         this.router.put("/payroll/update/:id", authenticateToken(['admin', 'doctor']), this.controller.updatePayroll.bind(this.controller));
         this.router.delete("/payroll/delete/:id", authenticateToken(['admin', 'doctor']), this.controller.deletePayroll.bind(this.controller));
     }
